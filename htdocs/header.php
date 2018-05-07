4ab85e4ad11e55df237e92c29d149d528083ee36
Piyush Tiwari <piyushtiwari1@gmail.com>
2018-05-07 16:16:12 +0530

    Bug fix for FTR

---
+++ htdocs/header.php
@@ -0,0 +1,30 @@
+<style>
+    #list li {
+        display: inline;
+        margin: 10px;
+        text-align: center;
+    }
+
+    #list a {
+        text-decoration: none;
+        color: green;
+    }
+
+    #list {
+        background-color: black;
+        width:  300px;
+        height: 25px;
+        border-radius: 5px;
+    }
+</style>
+
+<div id="list" >
+
+<ul>
+
+<li> <a href="#"> Sprint Report </a></li>
+<li > <a href="#"> Menu2 </a></li>
+<li > <a href="#"> Menu3 </a></li>
+</ul >
+
+</div >
